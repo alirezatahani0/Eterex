694993bcbdb5a4af145eb72c4e6dc8eda6952695
Alireza <alireza.tahani0@gmail.com>
2026-02-22 12:37:46 +0000

    change the campaign images

--- app/campaign/gads1404-s/page.tsx
+++ app/campaign/gads1404-s/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 const Page = () => {
 	return (
 		<div
-			className="h-screen w-full"
+			className="h-dvh w-full"
 			style={{
 				background: 'linear-gradient(180deg, #0b0c22,#1c1d40, #100f2c)',
 			}}
@@ -14,13 +14,13 @@ const Page = () => {
 				alt="banner"
 				width={1000}
 				height={1000}
-				className="w-full h-screen sm:w-fit sm:mx-auto"
+				className="w-full h-dvh sm:w-fit sm:mx-auto"
 			/>
 			<a
 				href="https://app.eterex.com/register"
 				target="_blank"
 				rel="noopener noreferrer"
-				className="absolute left-1/2 -translate-x-1/2 w-[80%] max-w-[420px] bottom-[12%] bg-[#0934f1] rounded-2xl flex flex-col items-center justify-center py-4 gap-2 gelatine cursor-pointer hover:opacity-95 transition-opacity"
+				className="absolute left-1/2 -translate-x-1/2 w-[80%] max-w-[420px] bottom-[10%] bg-[#0934f1] rounded-2xl flex flex-col items-center justify-center py-4 gap-2 gelatine cursor-pointer hover:opacity-95 transition-opacity"
 			>
 				<Text variant="Main/20px/Bold" className="text-white">
 					ثبت نام و شروع استیکینگ
--- app/campaign/gads1404-v/page.tsx
+++ app/campaign/gads1404-v/page.tsx
@@ -20,7 +20,7 @@ const Page = () => {
 				href="https://app.eterex.com/register"
 				target="_blank"
 				rel="noopener noreferrer"
-				className="absolute left-1/2 -translate-x-1/2 w-[80%] max-w-[420px] bottom-[12%] bg-[#0934f1] rounded-2xl flex flex-col items-center justify-center py-4 gap-2 gelatine cursor-pointer hover:opacity-95 transition-opacity"
+				className="absolute left-1/2 -translate-x-1/2 w-[80%] max-w-[420px] bottom-[10%] bg-[#0934f1] rounded-2xl flex flex-col items-center justify-center py-4 gap-2 gelatine cursor-pointer hover:opacity-95 transition-opacity"
 			>
 				<Text variant="Main/20px/Bold" className="text-white">
 					ثبت نام و شروع خرید و فروش
