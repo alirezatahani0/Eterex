2b38e95d757997f055e78d4c4eb3aa1f3065798e
Alireza <alireza.tahani0@gmail.com>
2026-02-22 11:52:10 +0000

    implement campaign 1404-v

--- app/campaign/gads1404-v/page.tsx
+++ app/campaign/gads1404-v/page.tsx
@@ -23,7 +23,7 @@ const Page = () => {
 				className="bg-[#0934f1] rounded-2xl flex flex-col items-center justify-center py-4 max-w-[80%] mx-auto gap-2 absolute left-0 right-0 sm:max-w-[420px] gelatine cursor-pointer hover:opacity-95 transition-opacity bottom-0 sm:bottom-40"
 			>
 				<Text variant="Main/24px/Bold" className="text-white">
-					ثبت نام و شروع استیکینگ
+					ثبت نام و شروع خرید و فروش
 				</Text>
 				<Text variant="Main/14px/Bold" className="text-white">
 					کمتر از ۵ دقیقه
